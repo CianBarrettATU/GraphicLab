0ad9c3da75df921486109be3463e985d60b4b710
CIAN BARRETT - STUDENT <g00416157@atu.ie>
2024-11-18 15:47:40 +0000

    loaded grayscale image

--- Lab8/lab8.py
+++ Lab8/lab8.py
@@ -4,13 +4,15 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('ATU.jpg',)
 
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 nrows = 2
 ncols = 1
 
 plt.subplot(nrows, ncols,1),plt.imshow(img, cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-#plt.subplot(nrows, ncols,2),plt.imshow(imgGray, cmap = 'gray')
-#plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,2),plt.imshow(imgGray, cmap = 'gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
 plt.show()
 
 
