9709e4411c5bb6f1f00e9cf6e3ad9206a3f6533d
CIAN BARRETT - STUDENT <g00416157@atu.ie>
2024-11-25 16:48:25 +0000

    harris corner detection

--- Lab9/lab9.py
+++ Lab9/lab9.py
@@ -2,22 +2,36 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('ATU.jpg',)
+img = cv2.imread('ATU1.jpg',)
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgHarris = img.copy()
+
+imgShiTomasi = img.copy()
 
 blockSize = 2
 apeture_size = 3
 k = 0.04
 
-imgHarris = imgGray.copy()
-
 dst = cv2.cornerHarris(imgGray, blockSize, apeture_size, k)
-
-plt.imshow(dst, cmap = "gray")
-plt.show()
 
 threshold = 0.5
 
-plt.imshow(imgHarris, cmap = "gray")
+B = 80
+G = 80
+R = 80 
+
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,(B, G, R),-1)
+
+maxCorners = np.max(imgHarris)
+
+qualityLevel = 0.01
+minDistance = 10
+
+corners = cv2.goodFeaturesToTrack(imgGray,maxCorners,qualityLevel,minDistance)
+
+plt.imshow(cv2.cvtColor(imgHarris, cv2.COLOR_BGR2RGB), cmap = "gray")
 plt.show()
