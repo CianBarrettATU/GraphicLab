f8fbddf6971819b88f42dfa6f5d5771c214bb55a
CIAN BARRETT - STUDENT <g00416157@atu.ie>
2024-12-02 15:26:56 +0000

    tracking objects

--- Lab9/lab9.py
+++ Lab9/lab9.py
@@ -90,8 +90,7 @@ def count_specific_classes(video_path, output_video_path, model_path, classes_to
     w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
     video_writer = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
-    line_points = [(20, 400), (1080, 400)]
-    counter = solutions.ObjectCounter(show=True, region=line_points, model=model_path, classes=classes_to_count)
+    counter = solutions.ObjectCounter(show=True, model=model_path, classes=classes_to_count)
 
     while cap.isOpened():
         success, im0 = cap.read()
