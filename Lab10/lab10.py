8849c4c983fc43339ac1b26b206f40726cc442ee
CIAN BARRETT - STUDENT <g00416157@atu.ie>
2024-12-02 16:53:41 +0000

    saved from end of lab

--- Lab10/lab10.py
+++ Lab10/lab10.py
@@ -1,9 +1,12 @@
 import cv2
+import numpy as np
+from collections import defaultdict
 
 from ultralytics import YOLO
 
 # Load the YOLO11 model
 model = YOLO("yolo11n.pt")
+track_history = defaultdict(lambda: [])
 
 # Open the video file
 video_path = "100m.mp4"
@@ -13,25 +16,38 @@ cap = cv2.VideoCapture(video_path)
 while cap.isOpened():
     # Read a frame from the video
     success, frame = cap.read()
-
     if success:
         # Run YOLO11 tracking on the frame, persisting tracks between frames
-        results = model.track(frame, persist=True)
-        for result in results:
-            boxes = result.boxes
-            if boxes.is_track:
-                track_ids = boxes.id
-                print(f"Tracking IDs: {track_ids}")
-            else:
-                print("Tracking is not enabled for these boxes.")
-
-        # Visualize the results on the frame
+        results = model.track(frame, persist=True, save=False)
+        boxes = results[0].boxes.xywh.cpu()
+        track_ids = results[0].boxes.id.int().cpu().tolist()
         annotated_frame = results[0].plot()
+        for box, track_id in zip(boxes, track_ids):
+            x, y, w, h = box
+            track = track_history[track_id]
+            track.append((float(x), float(y)))
+            #print("track:",track)
+            #print("X:",x)
+            #print("box:",box)
+            if len(track) > 30:
+                track.pop(0)
+            points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+            #print("points: ",points)
+
+            cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+
+            empt = np.array([[0, 0]])
+
+            difference = np.array(points[0] - points[-1])
+            
+            print(difference)
+            
+        # Visualize the results on the frame
         cv2.namedWindow('YOLOv11 Tracking', cv2.WINDOW_KEEPRATIO)
         # Display the annotated frame
         cv2.imshow("YOLOv11 Tracking", annotated_frame)
 
-        cv2.resizeWindow('YOLOv11 Tracking',1240, 700)
+        window = cv2.resizeWindow('YOLOv11 Tracking',1240, 700)
 
         # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord("q"):
